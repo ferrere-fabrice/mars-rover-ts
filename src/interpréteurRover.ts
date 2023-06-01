ab5feaf1c2733875971462a4960ac8d1cf374bc8
Enzo Sandré <atelier@enzosandre.fr>
2023-06-01 18:11:26 +0200

    Création d'un type commun Etat Rover rassemblant l'Orientation et la Position + Première implémentation idiote des commandes multiples

--- src/interpréteurRover.ts
+++ src/interpréteurRover.ts
@@ -1,5 +1,5 @@
 import {Rover} from "./rover.ts";
-import {Position} from "./geometrie/position.ts";
+import {EtatRover} from "./EtatRover.ts";
 
 export class InterpréteurRover {
     private _rover: Rover;
@@ -7,10 +7,16 @@ export class InterpréteurRover {
         this._rover = rover;
     }
 
+    public Interpréter(commande: string) : EtatRover {
+        if(commande == "DA"){
+            this._rover.TourneADroite();
+            return this._rover.Avancer();
+        }
 
-    public Interpréter(commande: string) : Position {
         if(commande == "A") return this._rover.Avancer();
         if(commande == "R") return this._rover.Reculer();
+        if(commande == "D") return this._rover.TourneADroite();
+        if(commande == "G") return this._rover.TourneAGauche();
 
         throw new Error();
     }
--- test/interpréteur.test.ts
+++ test/interpréteur.test.ts
@@ -4,6 +4,8 @@ import {CartesianData} from "./utilities/cartesianData";
 import {Position} from "../src/geometrie/position";
 import {PlanèteInfinie} from "./utilities/planèteInfinie";
 import {Point} from "../src/geometrie/point";
+import {Orientation} from "../src/topologie/orientation";
+import {TestPrimitives} from "./utilities/testPrimitives";
 const each = require("jest-each").default;
 
 const latitudesDépart = [0, 1];
@@ -44,5 +46,76 @@ describe('FEATURE Interpréteur', () => {
                 const résultatTémoin = roverTémoin.Reculer();
 
                 expect(résultatCommande).toStrictEqual(résultatTémoin);
-            })
+            });
+
+    each(new CartesianData(TestPrimitives.Orientations).toTestCases())
+        .it("ETANT DONNE un Interpréteur " +
+            "ET un Rover orienté %s " +
+            "QUAND on lui envoie 'D' " +
+            "ALORS le Rover tourne à droite",
+            (orientationDépart: Orientation) => {
+                const roverTémoin = new RoverBuilder().AyantPourOrientation(orientationDépart).Build();
+                const roverInterprété = new RoverBuilder().AyantPourOrientation(orientationDépart).Build();
+                const interpréteur = new InterpréteurRover(roverInterprété);
+
+                const résultatCommande = interpréteur.Interpréter("D");
+                const résultatTémoin = roverTémoin.TourneADroite();
+
+                expect(résultatCommande).toStrictEqual(résultatTémoin);
+            });
+
+    each(new CartesianData(TestPrimitives.Orientations).toTestCases())
+        .it("ETANT DONNE un Interpréteur " +
+            "ET un Rover orienté %s " +
+            "QUAND on lui envoie 'G' " +
+            "ALORS le Rover tourne à droite",
+            (orientationDépart: Orientation) => {
+                const roverTémoin = new RoverBuilder().AyantPourOrientation(orientationDépart).Build();
+                const roverInterprété = new RoverBuilder().AyantPourOrientation(orientationDépart).Build();
+                const interpréteur = new InterpréteurRover(roverInterprété);
+
+                const résultatCommande = interpréteur.Interpréter("G");
+                const résultatTémoin = roverTémoin.TourneAGauche();
+
+                expect(résultatCommande).toStrictEqual(résultatTémoin);
+            });
+
+    each(new CartesianData(["B", "VERFEFEF"]).toTestCases())
+        .it("ETANT DONNE un Interpréteur " +
+            "ET un Rover " +
+            "QUAND on lui envoie une commande invalide " +
+            "ALORS une Exception est lancée",
+            (commandeInvalide: string) => {
+                const interpréteur = new InterpréteurRover(new RoverBuilder().Build());
+
+                const résultatCommande = () => interpréteur.Interpréter(commandeInvalide);
+
+                expect(résultatCommande).toThrow()
+            });
+});
+
+describe('FEATURE Commandes Multiples', () => {
+    each(new CartesianData(TestPrimitives.Orientations, latitudesDépart, longitudesDépart).toTestCases())
+        .it("ETANT DONNE un Interpréteur " +
+            "ET un Rover orienté %s en position %s, %s " +
+            "QUAND on lui envoie 'DA' " +
+            "ALORS le Rover tourne à droite puis avance",
+            (orientation: Orientation, latitude: number, longitude: number) => {
+                const positionDépartCommune = new Position(new Point(latitude, longitude), new PlanèteInfinie());
+
+                const configurationCommune = new RoverBuilder()
+                    .AyantPourPosition(positionDépartCommune)
+                    .AyantPourOrientation(orientation);
+
+                const roverTémoin = configurationCommune.Build();
+                const roverInterprété = configurationCommune.Build();
+
+                const interpréteur = new InterpréteurRover(roverInterprété);
+
+                const résultatCommande = interpréteur.Interpréter("DA");
+                let résultatTémoin = roverTémoin.TourneADroite();
+                résultatTémoin = roverTémoin.Avancer();
+
+                expect(résultatCommande).toStrictEqual(résultatTémoin);
+            });
 });
